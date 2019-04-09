2a12f620dfae26f6e429b2ae22e1bd9086fc8c08
Pyrross <36333569+Pyrross@users.noreply.github.com>
2019-04-09 11:41:44 +0200

    fikset url-patterns med reset-funktion, så det passer

--- Parkeringshus/main/urls.py
+++ Parkeringshus/main/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, re_path
-from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "main"
@@ -9,9 +8,6 @@ urlpatterns = [
 	path('registrer/', views.register, name="registrer"),
 	path('logout/', views.logout_request, name="logout"),
 	path('login/', views.login_request, name="login"),
-	path('password-reset/',  auth_views.PasswordResetView.as_view(template_name = 'main/password_reset.html'), name="password_reset"),
-	path('password-reset/done/',  auth_views.PasswordResetDoneView.as_view(template_name = 'main/password_reset_done.html'), name="password_reset_done"),
-	path('password-reset-confirm/<uidb64>/<token>/',  auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 	path('support/', views.support, name="login"),
 	path('addplate/', views.addplate, name="addplate"),
 	re_path('addplate/(?P<pk>\d+)/delete/$', views.deleteplate.as_view(), name="delete_plate"),
