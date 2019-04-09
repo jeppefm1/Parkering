5f643509673f8bfcca0607b5b9eab276ffcc8342
jeppefm1 <42089396+jeppefm1@users.noreply.github.com>
2019-04-09 10:34:07 +0200

    Mindre rettelse af path

--- Parkeringshus/main/urls.py
+++ Parkeringshus/main/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	path('logout/', views.logout_request, name="logout"),
 	path('login/', views.login_request, name="login"),
 	path('password_reset/',  auth_views.PasswordResetView.as_view(template_name = 'templates/main/password_reset.html'), name="password_reset"),
-	path('password_reset_done/',  auth_views.PasswordResetDoneView.as_view(template_name = 'templates\main\password_reset_done.html'), name="password_reset_done"),
+	path('password_reset_done/',  auth_views.PasswordResetDoneView.as_view(template_name = 'templates/main/password_reset_done.html'), name="password_reset_done"),
 	path('password-rese-confirm/<uidb64>/<token>/',  auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 	path('support/', views.support, name="login"),
 	path('addplate/', views.addplate, name="addplate"),
