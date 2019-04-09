18b1e3a6ae6586cf32ce5d1fc3ce397970f269c6
Pyrross <36333569+Pyrross@users.noreply.github.com>
2019-04-09 13:45:06 +0200

    tilføjet login til stammen, væk fra main

--- Parkeringshus/Parkeringshus/urls.py
+++ Parkeringshus/Parkeringshus/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('main.urls')),
+	path('login/', views.login_request, name="login"),
 	path('password-reset/',  auth_views.PasswordResetView.as_view(template_name = 'main/password_reset.html'), name="password_reset"),
 	path('password-reset/done/',  auth_views.PasswordResetDoneView.as_view(template_name = 'main/password_reset_done.html'), name="password_reset_done"),
 	path('password-reset-confirm/<uidb64>/<token>/',  auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
--- Parkeringshus/main/urls.py
+++ Parkeringshus/main/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
 	path('', views.homepage, name="homepage"),
 	path('registrer/', views.register, name="registrer"),
 	path('logout/', views.logout_request, name="logout"),
-	path('login/', views.login_request, name="login"),
 	path('support/', views.support, name="login"),
 	path('addplate/', views.addplate, name="addplate"),
 	re_path('addplate/(?P<pk>\d+)/delete/$', views.deleteplate.as_view(), name="delete_plate"),
