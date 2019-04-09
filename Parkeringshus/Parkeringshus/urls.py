aeb7cc209013cf8f8424e4c8bee5a03919bd1436
Pyrross <36333569+Pyrross@users.noreply.github.com>
2019-04-09 13:46:25 +0200

    views i stamme urls.py

--- Parkeringshus/Parkeringshus/urls.py
+++ Parkeringshus/Parkeringshus/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('', include('main.urls')),
