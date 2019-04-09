b2861d0d9a793f5ce03dfa37515607640aaae2f3
jeppefm1 <42089396+jeppefm1@users.noreply.github.com>
2019-04-09 13:22:53 +0200

    Dropper enviroment

--- Parkeringshus/Parkeringshus/settings.py
+++ Parkeringshus/Parkeringshus/settings.py
@@ -180,5 +180,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
+EMAIL_HOST_USER = "parkeringtk@gmail.com"
+EMAIL_HOST_PASSWORD = "Vv43Pz@rD6bx"
--- Parkeringshus/Parkeringshus/wsgi.py
+++ Parkeringshus/Parkeringshus/wsgi.py
@@ -14,17 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Parkeringshus.settings')
 
 application = get_wsgi_application()
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
-
-sys.path.append(PROJECT_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
-
-import django.core.handlers.wsgi
-_application = django.core.handlers.wsgi.WSGIHandler()
-
-def application(environ, start_response):
-  os.environ['MY_DATABASE_NAME'] = environ['APP_DATABASE_NAME']
-  return _application(environ, start_response)
