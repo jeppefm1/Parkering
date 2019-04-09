46d879888bd34f79e7620a2b59a0cc6ee3768539
jeppefm1 <42089396+jeppefm1@users.noreply.github.com>
2019-04-09 13:16:21 +0200

    Password changes

--- Parkeringshus/Parkeringshus/wsgi.py
+++ Parkeringshus/Parkeringshus/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Parkeringshus.settings')
 
 application = get_wsgi_application()
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
+
+sys.path.append(PROJECT_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+
+import django.core.handlers.wsgi
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+def application(environ, start_response):
+  os.environ['MY_DATABASE_NAME'] = environ['APP_DATABASE_NAME']
+  return _application(environ, start_response)
