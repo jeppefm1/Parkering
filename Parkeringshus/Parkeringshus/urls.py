7e8ee88d9d80add3f3dcea67fb6af51539cafd72
jeppefm1 <42089396+jeppefm1@users.noreply.github.com>
2019-04-09 12:50:00 +0200

    Email

--- Parkeringshus/Parkeringshus/urls.py
+++ Parkeringshus/Parkeringshus/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
 	path('password-reset/',  auth_views.PasswordResetView.as_view(template_name = 'main/password_reset.html'), name="password_reset"),
 	path('password-reset/done/',  auth_views.PasswordResetDoneView.as_view(template_name = 'main/password_reset_done.html'), name="password_reset_done"),
 	path('password-reset-confirm/<uidb64>/<token>/',  auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password-reset-complete/',  auth_views.PasswordResetCompleteView.as_view(template_name = 'main/password_reset_complete.html'), name="password_reset_complete"),
     path('admin/', admin.site.urls),
 ]
